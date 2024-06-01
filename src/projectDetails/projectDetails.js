3fb83b8133379fcf0fe584e44743b97b0a90cd17
nikitakor7 <nikita.kor011@gmail.com>
2024-05-31 18:58:14 -0700

    Update projectDetails.js

--- src/projectDetails/projectDetails.js
+++ src/projectDetails/projectDetails.js
@@ -39,19 +39,10 @@ export function ProjectDetails() {
         <div className="d-project-info">
           <h2>Minimal Look Bedrooms</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquamsem
-            vitae turpis dignissim maximus. Aliquam sollicitudin tellumassa,
-            vbel maximus purus posuere in. Dojrices gravida dignissim. Praesent
-            at nibh in mi fringilla mattis. Phasellus ut dolor odio. Aenean in
-            the ipsum vel lectus bibendum commodo.
+            Experience the essence of simplicity and sophistication with our Minimal Look Bedrooms. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sem vitae turpis dignissim maximus. Aliquam sollicitudin tellus massa, vel maximus purus posuere in. Donec gravida dignissim. Present at nibh in mi fringilla mattis. Phasellus ut dolor odio. Aenean in ipsum vel lectus bibendum commodo.
             <br />
             <br />
-            In nec sem suscipit, convallis leo vitae, lacinia nibh. Cras amet
-            tellus lectus. Vivamus ipsum nunc, mattis quis nibh id, pellentesque
-            arcu. Donec a pellentesque Cras erat enim, gravida non ante
-            vitae,elequis convallis elit, in viverra felis. Donec ultrices
-            tellus vitae iaculisvd porta. Proin tincidunt ligula id purus
-            porttitor.
+            Immerse yourself in a space characterized by tranquility and refined aesthetics. In nec sem suscipit, convallis leo vitae, lacinia nibh. Cras amet tellus lectus. Vivamus ipsum nunc, mattis quis nibh id, pellentesque arcu. Donec a pellentesque. Cras erat enim, gravida non ante vitae, eleifend euismod lectus. Proin tincidunt ligula id purus porttitor.
           </p>
         </div>
       </div>
