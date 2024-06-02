8904a4db8d0a00c38c0521d52af92fdde11cd492
nikitakor7 <nikita.kor011@gmail.com>
2024-06-01 23:31:10 -0700

    Select image for projectcard

--- src/project/projectCard.js
+++ src/project/projectCard.js
@@ -1,5 +1,13 @@
 import "./projectCard.css";
 import project1 from "../images/project/project1.jpg";
+import project2 from "../images/project/project2.jpg";
+import project3 from "../images/project/project3.jpg";
+import project4 from "../images/project/project4.jpg";
+import project5 from "../images/project/project5.jpg";
+import project6 from "../images/project/project6.jpg";
+import project7 from "../images/project/project7.jpg";
+import project8 from "../images/project/project8.jpg";
+
 import { Link } from "react-router-dom";
 import { IoIosArrowForward } from "react-icons/io";
 
@@ -7,7 +15,9 @@ export function ProjectCard(props) {
     return(
         <div className="op-project">
           <div className="op-pro-img">
-            <img src={project1} alt="project"></img>
+            let ind = props.key;
+            let image_to_pick = `project${ind}`;
+            <img src=image_to_pick alt="project"></img>
           </div>
           <div className="op-pro-detail">
             <div className="op-pro-info">
