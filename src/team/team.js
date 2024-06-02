51f2de2e78ede314a9614b13b7a100150e1578a9
nikitakor7 <nikita.kor011@gmail.com>
2024-06-01 23:53:18 -0700

    Update team.js: Remove mistaken code

--- src/team/team.js
+++ src/team/team.js
@@ -1,42 +1,63 @@
-import "./projectCard.css";
-import project1 from "../images/project/project1.jpg";
-import project2 from "../images/project/project2.jpg";
-import project3 from "../images/project/project3.jpg";
-import project4 from "../images/project/project4.jpg";
-import project5 from "../images/project/project5.jpg";
-import project6 from "../images/project/project6.jpg";
-import project7 from "../images/project/project7.jpg";
-import project8 from "../images/project/project8.jpg";
-
+import "./team.css";
+import m1 from "../images/team/member1.png";
+import m0 from "../images/team/member2.jpg";
+import data from "./team_members.json";
+import { FaFacebookF, FaTwitter, FaLinkedin } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import { IoIosArrowForward } from "react-icons/io";
 
-const projectImages = [project1, project2, project3, project4, project5, project6, project7, project8];
+const images = {
+  "member1.png": m0,
+  "member2.jpg": m1,
+  // Add more image imports and mappings if necessary
+};
 
-export function ProjectCard(props) {
-    const imageIndex = parseInt(props.key); // Assuming props.key starts from 1
-    const imageSrc = projectImages[imageIndex];
-    
-    return(
-        <div className="op-project">
-          <div className="op-pro-img">
-            {/* Correctly set the src attribute with the dynamic image source */}
-            <img src={imageSrc} alt="project" />
-          </div>
-          <div className="op-pro-detail">
-            <div className="op-pro-info">
-              <p className="op-prj-title">{props.props.project_name}</p>
-              {/* {props.data.projects.projectName} */}
-              <p className="op-prj-path">Decor / Architecture</p>
-            </div>
-            <div className="op-pro-btn">
-              <Link to={`/project-details`}>
-                <button>
-                  <IoIosArrowForward />
-                </button>
+export function Team() {
+  return (
+    <div className="team">
+      <div className="team-header">
+        <h1>
+          Our Professional
+          <p>Home / Team</p>
+        </h1>
+      </div>
+      <div className="team-members">
+        {data.members
+          .filter((m, ind) => ind < 8)
+          .map((mbr, i) => {
+            return (
+              <Link to={`/team-single`} key={i}>
+                <div className="member">
+                  <div className="member-photo">
+                    <img src={images[mbr.photo]} alt={mbr.fullname}></img>
+                  </div>
+                  <div className="member-info">
+                    <p className="fullname">{mbr.fullname}</p>
+                    <p className="m-detail">
+                      {mbr.job}, {mbr.country}
+                    </p>
+                    <ul>
+                      <li>
+                        <a href="https://www.facebook.com/">
+                          <FaFacebookF />
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://www.linkedin.com/">
+                          <FaLinkedin />
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://www.twitter.com/">
+                          <FaTwitter />
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </Link>
-            </div>
-          </div>
-        </div>
-    );
+            );
+          })}
+      </div>
+    </div>
+  );
 }
